6d949db5eeb40fa6978d1789572753b264fec1a3
Vicente Obregon <desarrollogis@gmail.com>
2013-10-23 00:34:23 -0700

    initial

---
+++ minimal_wp_plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: minimal_wp_plugin 1.0
+ * Plugin URI: http://desarrollogis.dyndns.org/
+ * Description: minimal_wp_plugin 1.0
+ * Version: 1.0
+ * Author: desarrollogis
+ * Author URI: http://desarrollogis.dyndns.org/
+ */
+?>
